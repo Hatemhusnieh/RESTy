5e09fae9711cffe0e8816bd3ad5ec89367c628c4
hatem <hatemhtm7@gmail.com>
2021-08-10 00:55:44 +0300

    lab 27

--- README.md
+++ README.md
@@ -9,25 +9,35 @@
 1. download independencies by `npm i`
 1. run the app
 
-## Deploy, Run and Test
+## Deploy, Run and Test - Version 1.0.0
 
-- [deployed link]()
 - [Pull Request](https://github.com/Hatemhusnieh/RESTy/pull/1)
 - [SandBox](https://codesandbox.io/s/magical-golick-4x3cu?file=/resty/src/App.js)
 
 ### Setup
 
-#### `.env` requirement
-
-- `PORT` - port number
-- `MONGODB_URL` - port number
-- `SECRET` - a random secret for encryption
-
 #### Running the app
 
-- either:
-  1. `npm start`
+- `npm start`
 
 ### UML:
 
 ![uml](resources/uml-1.jpg)
+
+---
+
+## Deploy, Run and Test - Version 1.1.0
+
+- [Deployed Link]()
+- [Pull Request]()
+- [SandBox]()
+
+### Setup
+
+#### Running the app
+
+- `npm start`
+
+### UML:
+
+![uml](resources/useState-Hook.jpg)
--- resty/src/App.js
+++ resty/src/App.js
@@ -1,43 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
 import './app.scss';
 import Header from './components/header/Header.jsx';
 import Footer from './components/footer/Footer.jsx';
 import Form from './components/form/Form.jsx';
 import Results from './components/results/Results.jsx';
+import { BeatLoader } from 'react-spinners';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
+function App() {
+  const [data, setData] = useState(null);
+  const [requestParams, setRequestParams] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  function callApi(formData, resData) {
+    setData(resData);
+    setRequestParams(formData);
   }
-
-  callApi = (requestParams) => {
-    // mock output
-    const data = {
-      count: 2,
-      results: [
-        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
-        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
-      ],
-    };
-    this.setState({ data, requestParams });
-  };
-
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div>Request Method: {this.state.requestParams.method}</div>
-        <div>URL: {this.state.requestParams.url}</div>
-        <Form handleApiCall={this.callApi} />
-        {this.state.data && <Results data={this.state.data} />}
-        <Footer />
-      </React.Fragment>
-    );
+  function handleLoad(load) {
+    setLoading(load);
   }
+  return (
+    <React.Fragment>
+      <Header />
+      <div>Request Method: {requestParams.method}</div>
+      <div>URL: {requestParams.url}</div>
+      <Form handleApiCall={callApi} handleLoading={handleLoad} />
+      {loading ? <BeatLoader loading /> : <Results data={data} />}
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default App;
