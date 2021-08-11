d124c978933c4f6af069b6246bd91fd2730596de
hatem <hatemhtm7@gmail.com>
2021-08-12 02:12:11 +0300

    lab 28

--- resty/src/App.js
+++ resty/src/App.js
@@ -1,28 +1,38 @@
 import React from 'react';
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './app.scss';
 import Header from './components/header/Header.jsx';
 import Footer from './components/footer/Footer.jsx';
 import Form from './components/form/Form.jsx';
 import Results from './components/results/Results.jsx';
+import History from './components/history/History';
 import { BeatLoader } from 'react-spinners';
 
 function App() {
   const [data, setData] = useState(null);
   const [requestParams, setRequestParams] = useState({});
+  const [history, setHistory] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  async function callApi(formData, resData) {
-    this.handleLoading(true);
-    try {
-      const res = await fetch(formData.url);
-      const data = await res.json();
-      setData({ data });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    if (requestParams.url) {
+      const data = await axios[requestParams.method](requestParams.url);
+      setData(data);
+      setLoading(false);
+    }
+  }, [requestParams]);
+
+  async function callApi(formData) {
+    setLoading(true);
+    if (formData.url) {
       setRequestParams(formData);
-      this.handleLoading(false);
-    } catch (error) {
+      setHistory([...history, formData]);
+    } else {
       const data = {
         count: 2,
+        method: formData.method,
         results: [
           { name: 'fake thing 1', url: 'http://fakethings.com/1' },
           { name: 'fake thing 2', url: 'http://fakethings.com/2' },
@@ -30,21 +40,19 @@ function App() {
       };
       setData({ data });
       setRequestParams(formData);
-      this.handleLoading(false);
+      setHistory([...history, formData]);
+      setLoading(false);
     }
-  }
-
-  function handleLoad(load) {
-    setLoading(load);
   }
 
   return (
     <React.Fragment>
       <Header />
-      <div>Request Method: {requestParams.method}</div>
+      <div>Request Method: {requestParams.method?.toUpperCase()}</div>
       <div>URL: {requestParams.url}</div>
-      <Form handleApiCall={callApi} handleLoading={handleLoad} />
-      {loading ? <BeatLoader loading /> : <Results data={data} />}
+      <Form handleApiCall={callApi} />
+      {history.length ? <History history={history} /> : null}
+      {loading ? <BeatLoader loading /> : data && <Results data={data} />}
       <Footer />
     </React.Fragment>
   );
--- resty/src/components/results/Results.jsx
+++ resty/src/components/results/Results.jsx
@@ -2,9 +2,18 @@ import JSONPretty from 'react-json-pretty';
 import 'react-json-pretty/themes/monikai.css';
 
 function Results(props) {
+  const mock = {
+    'type': `mock ${props.data.data.method} data`,
+    'cache-control': 'public, max-age=86400, s-maxage=86400',
+    'content-type': 'application/json; charset=utf-8',
+  };
+
   return (
     <section>
-      <JSONPretty id="json-pretty" data={props.data}></JSONPretty>
+      <h2>Headers</h2>
+      <JSONPretty data={props.data.headers ? props.data.headers : mock}></JSONPretty>
+      <h2>Results</h2>
+      <JSONPretty data={props.data.data.results}></JSONPretty>
     </section>
   );
 }
