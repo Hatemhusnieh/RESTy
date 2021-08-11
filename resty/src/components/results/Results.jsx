5b9786c1bca1d7e7c6f1c08a29be798bc953c878
hatem <hatemhtm7@gmail.com>
2021-08-11 19:31:58 +0300

    deployment

--- resty/src/components/results/Results.jsx
+++ resty/src/components/results/Results.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import JSONPretty from 'react-json-pretty';
+import 'react-json-pretty/themes/monikai.css';
 
 function Results(props) {
   return (
     <section>
-      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+      <JSONPretty id="json-pretty" data={props.data}></JSONPretty>
     </section>
   );
 }
