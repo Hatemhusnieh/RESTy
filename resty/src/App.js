5ba2ac5e87245101c4436741a38ed32807df55f9
hatem <hatemhtm7@gmail.com>
2021-08-16 12:22:20 +0300

    history

--- resty/src/App.js
+++ resty/src/App.js
@@ -5,16 +5,19 @@ import Header from './components/header/Header.jsx';
 import Footer from './components/footer/Footer.jsx';
 import Form from './components/form/Form.jsx';
 import Results from './components/results/Results.jsx';
+import History from './components/history/History';
 import { BeatLoader } from 'react-spinners';
 
 function App() {
   const [data, setData] = useState(null);
   const [requestParams, setRequestParams] = useState({});
+  const [history, setHistory] = useState([]);
   const [loading, setLoading] = useState(false);
 
   function callApi(formData, resData) {
     setData(resData);
     setRequestParams(formData);
+    setHistory([...history, formData]);
   }
   function handleLoad(load) {
     setLoading(load);
@@ -25,6 +28,7 @@ function App() {
       <div>Request Method: {requestParams.method}</div>
       <div>URL: {requestParams.url}</div>
       <Form handleApiCall={callApi} handleLoading={handleLoad} />
+      {history.length ? <History history={history} /> : null}
       {loading ? <BeatLoader loading /> : <Results data={data} />}
       <Footer />
     </React.Fragment>
